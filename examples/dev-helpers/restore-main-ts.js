b9cc9a0040d35553b08128ccd0b87bb35592401c
max <maxim.maximov@netcracker.com>
2020-04-13 11:05:59 +0300

    update

--- examples/dev-helpers/restore-main-ts.js
+++
@@ -1,8 +0,0 @@
-// MUI builder removes data from main.ts to create plugins, hence we need to return it
-
-const fs = require("fs");
-const path = require("path");
-
-fs.writeFileSync(path.resolve("./rktn-fragments/src/main.ts"), `
-    Some files
-`);
